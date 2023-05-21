c248fea96e5829533872b89b9caea09d2e9d8d0d
Ansh Deshraj <126981356+anshhdeshraj@users.noreply.github.com>
2023-05-21 23:45:42 +0530

    Update SettingsScreen.js

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import settingsStyle from '../styles/settingsStyle';
 
 const SettingsScreen = ({navigation}) => {
-    const avatarURL = 'https://plus.unsplash.com/premium_photo-1683817397956-b46614758fb8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+    const avatarURL = '#'
     
     const [username, setUsername] = useState('')
 
